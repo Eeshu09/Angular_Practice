80110f9f4e268bd713f2c67363212cdaae3061a7
Eeshu <eeshupratap25@gmail.com>
2025-05-20 19:25:17 +0530

    ---implement pagenotfound functionality and lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { DirectiveComponent } from './pages/directive/directive.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { ProductQueryComponent } from './pages/product-query/product-query.component';
+import { FormvalidationComponent } from './pages/formvalidation/formvalidation.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -18,5 +20,12 @@ export const routes: Routes = [
     {
         path:'productDetails/:id',component:ProductDetailsComponent
     },
-    {path:'product_query',component:ProductQueryComponent}
+    {path:'product_query',component:ProductQueryComponent},
+    {path:'formvalidaition',loadComponent:()=>import('../app/pages/formvalidation/formvalidation.component').then((c)=>c.FormvalidationComponent)
+        
+    },
+    // {
+    //     path:'formvalidaition',component:FormvalidationComponent
+    // }
+    {path:'**',component:PageNotFoundComponent,title:"NotFounds"}
 ];
---
+++ src/app/pages/formvalidation/formvalidation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formvalidation',
+  imports: [],
+  templateUrl: './formvalidation.component.html',
+  styleUrl: './formvalidation.component.css'
+})
+export class FormvalidationComponent {
+
+}
